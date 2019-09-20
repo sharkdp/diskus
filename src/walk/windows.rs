05cd00b37ce4042caf0d5cafa4f2c3b186e8bc73
sharkdp <davidpeter@web.de>
2019-09-20 21:57:41 +0200

    Shorten explanation

--- src/walk/windows.rs
+++ src/walk/windows.rs
@@ -1,13 +1,9 @@
 pub fn generate_unique_id(_metadata: &std::fs::Metadata) -> Option<super::UniqueID> {
-    // Since even the Windows-internal tools such as (but not limited to)
-    // - Powershell,
-    // - Explorer,
-    // - dir,
-    // are not respecting hardlinks or junction points when determining the
-    // size of a directory [1], it has been decided that diskus will count
-    // any such entries multiple times. too.
+    // Windows-internal tools such as Powershell, Explorer or `dir` are not respecting hardlinks
+    // or junction points when determining the size of a directory. `diskus` does the same and
+    // counts such entries multiple times (on Unix systems, multiple hardlinks to a single file are
+    // counted just once).
     //
-    // Footnotes:
-    // [1] https://github.com/sharkdp/diskus/issues/32#issuecomment-532817905
+    // See: https://github.com/sharkdp/diskus/issues/32
     None
 }
